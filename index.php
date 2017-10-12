f46e11dbb29f529cee11205c3357299f62fab108
Barakial <Barakiall@gmail.com>
2017-10-12 11:11:15 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+
+** Contacts developer
+
+* @developer Barakial
+* @skype vega1eugene
+* @site Barakial.com
+*/
+?>
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Webpr | Demo site wp-theme</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <script type="text/javascript" src="js/plugin/jquery-2.1.1.min.js"></script>
+
+</head>
+<body>
+<header>
+
+</header>
+
+<footer></footer>
+<script type="text/javascript" src="js/plugin/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/plugin/jquery.magnific-popup.min.js"></script>
+<script type="text/javascript" src="js/plugin/owl.carousel.min.js"></script>
+<script type="text/javascript" src="js/plugin/jquery.maskedinput.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
+</body>
+</html>
